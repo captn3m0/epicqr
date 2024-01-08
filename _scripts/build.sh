4882c0c09b0f24dcaafe7b082ee09629b7b771c2
Nemo <me@captnemo.in>
2024-01-08 10:53:49 +0530

    move target hxmls away from root

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -4,10 +4,14 @@ rm -f haxe-build.zip
 zip -r haxe-build.zip src LICENSE *.md *.json *.hxml
 
 # Java
+# TODO: Directly generate JAR from haxe
 javac -cp src src/eci/*.java -d build/
 jar -cf epicqr-release.jar build/*
 
 # PHP
-haxe php.hxml
+haxe _targets/php.hxml
 sed -i '/^use/d' src/eci/*.php
 sed -i '/^Boot::/d' src/eci/*.php
+
+# Node.js
+haxe _targets/nodejs.hxml
